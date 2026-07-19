48ec4418b32314649ed3fb92faef752195b50f6c
Monica Chen <monicachen@berkeley.edu>
2026-07-19 07:29:03 +0000

    part 1 done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil?
+      all
+    else
+      self.where(rating: ratings_list)
+    end
+  end
 end
